387c94ad4ea0db2a3baebd973799d9c9b76e8e23
Kirill Nikitin <feranic@yandex.ru>
2020-08-23 18:43:14 +0300

    Done second window

--- main.py
+++ main.py
@@ -4,9 +4,24 @@ import db
 
 def main():
     layout = [
-        [PySimpleGUI.Ok()]
+        [PySimpleGUI.Button("Добавить")],
+         [PySimpleGUI.Ok(), PySimpleGUI.Cancel()]
     ]
-    window = PySimpleGUI.Window('Главное окно', layout).read()
+    layout1 = [[PySimpleGUI.Ok()]]
+    window = PySimpleGUI.Window('Главное окно', layout)
+    db_driver = db.DB("pay_planner2_db.db")
+    while True:
+        event, values = window.read()
+        if event in (None, 'Exit', 'Cancel'):
+            break
+        if event == "Добавить":
+            window1 = PySimpleGUI.Window("Окно 1", layout1)
+            while True:
+                event, values = window1.read()
+                if event in (None, 'Ok'):
+                    break
+            window1.close()
+    window.close()
 
 
 if __name__=="__main__":
