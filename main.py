318179f430815d2d349b4babe4a562e4686e6327
Kirill Nikitin <feranic@yandex.ru>
2020-09-11 09:58:11 +0300

    Реализовал формирование раскладки формы добавления записи layout1 в виде отдельной функции.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from util import Util
 
 def main():
     db_driver = db.DB("pay_planner2_db.db")
+    layout1 = Util.layout1_setup(db_driver)
     table_data = Util.make_basic_table(db_driver)
     header_list = ["Название сервиса", "Состояние подписки",
                    "Период продления","Сумма", "Срок окончания"]
@@ -21,35 +22,11 @@ def main():
         event, values = window.read(timeout=100)
         if event in (None, 'Exit', 'Cancel'):
             break
-        if event == "_table_":
-            layout1 = [[psg.Ok()]]
-            window1 = psg.Window("Окно 1", layout1)
-            while True:
-                event, values = window1.read()
-                if event in (None, 'Ok'):
-                    break
-            window1.close()
         if event == "_addbutton_":
-            states_list = []
-            duration_list = []
-            durations_from_db = db_driver.get_all_durations()
-            states_from_db = db_driver.get_all_states()
-            for st in range(0, len(states_from_db)):
-                states_list.append(str(states_from_db[st][0]))
-            for dur in range(0, len(durations_from_db)):
-                duration_list.append(str(durations_from_db[dur][0]))
-            layout2 = [[psg.Text("Название подписки:"), psg.Input(key="_subscription_")],
-                       [psg.Text("Статус:"), psg.Combo(states_list, default_value=states_list[0], key="_state_"),
-                        psg.Text("Срок продления:"), psg.Combo(duration_list, default_value=duration_list[0], key="_duration_"),
-                       psg.Text("мес.")],
-                         [psg.Text("Сумма списания:"), psg.Input(key="_price_")],
-                        [psg.Text("Срок окончания:"), psg.Input(disabled=True, key="_ending_"),
-                         # psg.CalendarButton("Выбрать дату", target="_ending_", key="_termend_", format="%Y-%m-%d")],
-                         psg.Button("Выбрать дату", key="_termend_")],
-                          [psg.Button("Сохранить")]]
-            window2 = psg.Window("Добавление подписки", layout2)
+            layout1 = Util.layout1_setup(db_driver)
+            window1 = psg.Window("Добавление подписки", layout1)
             while True:
-                event, values = window2.read(timeout=100)
+                event, values = window1.read(timeout=100)
                 if event in (None, 'Exit'):
                     break
                 if event == "_termend_":
@@ -58,7 +35,7 @@ def main():
                     day = "0" + str(date[1]) if len(str(date[1])) == 1 else str(date[1])
                     year = str(date[2])
                     date_in_format = year + "-" + month + "-" + day
-                    window2["_ending_"](date_in_format)
+                    window1["_ending_"](date_in_format)
                 if event == "Сохранить":
                     if (values["_subscription_"] == "") or \
                     (values["_price_"] == "") or \
@@ -75,7 +52,15 @@ def main():
                     db_driver.add_subscription_to_db(tuple_to_add)
                     psg.Popup("Добавление", "Новая подписка успешно добавлена!")
                     window["_table_"](Util.make_basic_table(db_driver))
-            window2.close()
+            window1.close()
+            
+        if event == "_editbutton_" or event == "_table_":
+            if len(values["_table_"]) == 0:
+                psg.Popup("Ошибка", "Не выбрана запись для редактирования")
+                continue
+            layout2 = layout1
+            window2 = psg.Window("Редактирование записи", layout2)
+                
         if event == "_deletebutton_":
             if len(values["_table_"]) == 0:
                 psg.Popup("Ошибка", "Не выбрана запись для удаления")
--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+import PySimpleGUI as psg
 class Util:
     def make_basic_table(db_driver):
         table_data = db_driver.get_subs_for_table()
@@ -14,3 +15,22 @@ class Util:
             new_table_data.append(tup)
         table_data = new_table_data
         return table_data
+
+    def layout1_setup(db_driver):
+        states_list = []
+        duration_list = []
+        durations_from_db = db_driver.get_all_durations()
+        states_from_db = db_driver.get_all_states()
+        for st in range(0, len(states_from_db)):
+            states_list.append(str(states_from_db[st][0]))
+        for dur in range(0, len(durations_from_db)):
+            duration_list.append(str(durations_from_db[dur][0]))
+        return [[psg.Text("Название подписки:"), psg.Input(key="_subscription_")],
+               [psg.Text("Статус:"), psg.Combo(states_list, default_value=states_list[0], key="_state_"),
+                psg.Text("Срок продления:"), psg.Combo(duration_list, default_value=duration_list[0], key="_duration_"),
+                psg.Text("мес.")],
+               [psg.Text("Сумма списания:"), psg.Input(key="_price_")],
+               [psg.Text("Срок окончания:"), psg.Input(disabled=True, key="_ending_"),
+                # psg.CalendarButton("Выбрать дату", target="_ending_", key="_termend_", format="%Y-%m-%d")],
+                psg.Button("Выбрать дату", key="_termend_")],
+               [psg.Button("Сохранить")]]
